0f6c59541ea2790606fbedacc37d5e520d63fa74
kinjoyuji <kinjoyuji@gmail.com>
2017-08-12 12:26:11 +0900

    productListをリファクタリング

--- src/react-components/Product/ProductList.js
+++ src/react-components/Product/ProductList.js
@@ -5,24 +5,11 @@ class ProductList extends React.Component {
   render(){
     return(
       <ul className='product-list'>
-        <ProductItem
-          id = {this.props.productList[0].id}
-          name = {this.props.productList[0].name}
-          link = {this.props.productList[0].link}
-          media = {this.props.productList[0].media}
-          upvote = {this.props.productList[0].upvote}
-          description = {this.props.productList[0].description}
-          maker = {this.props.productList[0].maker}
-          />
-        <ProductItem
-          id = {this.props.productList[1].id}
-          name = {this.props.productList[1].name}
-          link = {this.props.productList[1].link}
-          media = {this.props.productList[1].media}
-          upvote = {this.props.productList[1].upvote}
-          description = {this.props.productList[1].description}
-          maker = {this.props.productList[1].maker}
-          />
+        {
+          this.props.productList.map(function(item, idx){
+            return <ProductItem key={idx} {...item}/>
+          });
+        }
       </ul>
     );
   }
