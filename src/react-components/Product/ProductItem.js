70befdc91dfad8101a37f65396a1fdfcd0dabff9
kinjoyuji <kinjoyuji@gmail.com>
2017-08-12 12:33:34 +0900

    ProductItemをリファクタリング

--- src/react-components/Product/ProductItem.js
+++ src/react-components/Product/ProductItem.js
@@ -1,30 +1,47 @@
 import React from 'react';
 
 class ProductItem extends React.Component {
+  renderUpVoteButton(){
+    return(
+      <a className="upvote-button" href='#'>
+        <span>
+          <i className="fa fa-sort-asc"></i >
+        </span>
+        <br/>
+        {this.props.upvote}
+      </a>
+    );
+  }
+
+  renderNewWindowIcon(){
+    return(
+      <a className="prodct-item-link" href={this.props.link}>
+        <span>
+          <i className="fa fa-external-link"></i >
+        </span>
+      </a>
+    );
+  }
+
+  renderInfoSection(){
+    return(
+      <section className="product-item-info">
+        <a href="#">
+          <h2>{this.props.name}</h2>
+        </a>
+        <p>
+          {this.props.description}
+        </p>
+      </section>
+    );
+  }
   render(){
     return(
       <li className='product-item'>
-        <a className="upvote-button" href='#'>
-          <span>
-            <i className="fa fa-sort-asc"></i >
-          </span>
-          <br/>
-          {this.props.upvote}
-        </a>
+        {this.renderUpVoteButton()}
         <img className="product-item-media" src={this.props.media} />
-        <section className="product-item-info">
-          <a href="#">
-            <h2>{this.props.name}</h2>
-          </a>
-          <p>
-            {this.props.description}
-          </p>
-        </section>
-        <a className="prodct-item-link" href={this.props.link}>
-          <span>
-            <i className="fa fa-external-link"></i >
-          </span>
-        </a>
+        {this.renderInfoSection()}
+        {this.renderNewWindowIcon()}
       </li>
     );
   }
