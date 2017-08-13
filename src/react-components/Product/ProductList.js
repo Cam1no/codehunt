afa9402f7c15541abdb72babf0f14d8c4b7a7f2a
kinjoyuji <kinjoyuji@gmail.com>
2017-08-13 15:29:37 +0900

    fix

--- src/react-components/Product/ProductList.js
+++ src/react-components/Product/ProductList.js
@@ -7,8 +7,8 @@ class ProductList extends React.Component {
       <ul className='product-list'>
         {
           this.props.productList.map(function(item, idx){
-            return <ProductItem key={idx} pid={item.key} {...item}/>
-          });
+            return <ProductItem key={idx} pid={item.key} {...item}/>;
+          })
         }
       </ul>
     );
